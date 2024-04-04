7b71a7fd78066df7129d87d0018b601fa146c897
Sendus <118221958+SendG@users.noreply.github.com>
2024-04-04 11:46:53 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require('express')// frame work
 
 const app = express()
 
+app.get('/', (req,res)=>(console.log(" hello github"
+
+)))
+
 
 app.listen(PORT,()=> console.log(`server running on port-> ${PORT}`))
 
