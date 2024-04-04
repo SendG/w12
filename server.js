8c15d73abe9fd20c258fdd2399c0ac99a349dbe0
Your Name <sendusgeo@gmail.com>
2024-04-04 11:24:48 -0400

    initcommit

---
+++ server.js
@@ -0,0 +1,9 @@
+// need to do the project starts tomorrow march 262024 
+const express = require('express')// frame work 
+
+const app = express()
+
+
+app.listen(PORT,()=> console.log(`server running on port-> ${PORT}`))
+
+
